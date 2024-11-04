25738f8085f9220567715745d66b25d0b3086cc7
Jamie L <jamielong45@hotmail.com>
2024-11-04 22:19:17 +0000

    single responsibility

--- src/single-responsibility.ts
+++ src/single-responsibility.ts
@@ -12,22 +12,22 @@ class Teapot {
 
 class Cup {
   pour(teaType: string): string {
-    console.log(`Pouring ${teaType} tea into a cup...`);
+    console.log(`Pouring tea into a cup...`);
     return(`Cup of ${teaType} tea!`);
   }
 }
 
-function serveTea(waterAmount: number, teaType: string): void {
+function serveTea(waterAmount: number, teaType: string) {
   const kettle = new Kettle();
   const teapot = new Teapot();
   const cup = new Cup();
 
   kettle.boilWater(waterAmount);
   teapot.brewTea(teaType);
-  const pouredTea = cup.pour(teaType);
+  const cupOfTea = cup.pour(teaType);
 
-  console.log(pouredTea);
+  console.log(cupOfTea);
 }
 
 serveTea(250, 'green');
-serveTea(275, 'black');
+serveTea(275, 'herbal');
